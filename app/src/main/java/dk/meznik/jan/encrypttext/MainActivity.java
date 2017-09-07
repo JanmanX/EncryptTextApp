6affd782316cde2a9cac69ef76934bf357cde5a6
Jan Meznik <jan@meznik.dk>
2017-09-07 21:15:08 +0200

    Don't show the old layout on startup.

--- app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
+++ app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
 
         Intent intent = new Intent(this, EncryptActivity.class);
