e30fdeeb18324d78fe3abbb38fae7d633fac377c
Taco <SkytkRSfan3895@gmail.com>
2019-01-01 20:15:24 -0500

    migrate to androidx
    Signed-off-by: Taco <SkytkRSfan3895@gmail.com>

--- app/src/main/java/dk/meznik/jan/encrypttext/EncryptActivity.java
+++ app/src/main/java/dk/meznik/jan/encrypttext/EncryptActivity.java
@@ -5,7 +5,9 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
+++ app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
@@ -2,7 +2,8 @@ package dk.meznik.jan.encrypttext;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
